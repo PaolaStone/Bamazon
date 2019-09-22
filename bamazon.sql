c19a65c8b6063d8796d45a7e645b7b569a5bdbe4
Paola Stone <paola.stonemartinez@dell.com>
2019-09-22 14:43:09 -0400

    completed

--- bamazon.sql
+++ bamazon.sql
@@ -1,18 +1,16 @@
 DROP DATABASE IF EXISTS bamazonDB;
 CREATE DATABASE bamazonDB;
-​
-USE bamazonDB;
-​
-CREATE TABLE products(
+​USE bamazonDB;
+​CREATE TABLE products(
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NOT NULL,
-  departmemt_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
   price DECIMAL(10,2) NOT NULL,
   stock_quantity INT,
   PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, departmemt_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES 
   ("pen", "office", 3.50, 100),
   ("eraser", "office", 2.50, 100),
@@ -24,5 +22,3 @@ VALUES
   ("coockies", "food", 3, 100),
   ("orange", "food", 1, 100),
   ("cereal", "food", 6, 100)
-
-
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,27 +17,115 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  displayAll()
-  });
+  displayAll();
+});
+
+
 
 
 function displayAll() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
+      console.log("");
+      console.log("                    Inventory")
       console.log("---------------------------------------------------");
-      console.log("Inventory")
-      console.log("---------------------------------------------------");
-      console.log("Item ID | Product | Department | Price | Available ")
-      console.log("---------------------------------------------------");
-
+      console.log("");
+      
       for (var i = 0; i < res.length; i++) {
-        console.log(res[i].item_id + " | " + 
-        res[i].product_name + " | " + 
-        res[i].departmemt_name + " | " + 
-        res[i].price + " | " + 
-        res[i].stock_quantity);
+        console.log("Item ID:       " + res[i].item_id); 
+        console.log("Product:       " + res[i].product_name); 
+        console.log("Department:    " + res[i].department_name); 
+        console.log("Price:         $" + res[i].price.toFixed(2));
+        console.log("Qty in stock:  " + res[i].stock_quantity);
+        console.log("----------------------------------------------------");
     }
     console.log("----------------------------------------------------");
-    connection.end();
+    console.log("");
+    customerChoice();
+    // connection.end();
   });
 };
+
+function customerChoice() {
+  inquirer
+    .prompt([
+      {
+        name: "item_id",
+        type: "number",
+        message: "Please enter the ID of the product you would like to buy  ",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          // return false;
+          connection.end();
+          console.log("");
+          console.log("This is not a valid entry, please start process from beginning");
+        }
+      },
+      {
+        name: "quantity",
+        type: "number",
+        message: "How many would you like to buy?  ",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          // return false;
+          connection.end();
+          console.log("");
+          console.log("This is not a valid entry, please start process from beginning");
+        }
+      }
+    ])
+    .then(function(answer) {
+      var query = "SELECT item_id, product_name, product_name, price, stock_quantity FROM products WHERE ?";
+      connection.query(query, { item_id: answer.item_id}, function(err, res) {
+        if (err) throw err;
+          for (var i = 0; i < res.length; i++) {
+            console.log("")
+            console.log(" This is what we have in inventory for this product")
+            console.log("---------------------------------------------------");
+            console.log("Product:       " + res[i].product_name);
+            console.log("Price:         $" + res[i].price.toFixed(2));
+            console.log("Qty. in stock: " + res[i].stock_quantity);
+            console.log("");
+            if (answer.quantity > res[i].stock_quantity){
+              console.log("---------------------------------------------------")
+              console.log("***Sorry, we do not have the quantity you need***")
+              console.log("---------------------------------------------------")
+              connection.end();
+            }else{
+              customerTotal = answer.quantity * res[i].price
+              quantityUpdate = res[i].stock_quantity - answer.quantity
+              console.log("")
+              console.log("---------------------------------------------------");
+              console.log("This is the total of your purchase: $" + customerTotal);
+              console.log("---------------------------------------------------");
+              connection.query(
+                "UPDATE products SET ? WHERE ?",
+                  [
+                    {
+                      stock_quantity: quantityUpdate
+                    },
+                    {
+                      item_id: answer.item_id
+                    }
+                  ],
+              );
+                console.log("")
+                console.log("")
+                console.log(" This is what we have in inventory after this purchase")
+                console.log("---------------------------------------------------");
+                console.log("Product:       " + res[i].product_name);
+                console.log("Price:         $" + res[i].price.toFixed(2));
+                console.log("Qty. in stock: " + quantityUpdate);
+                console.log("");
+                connection.end();
+              
+            }
+          
+        };
+        
+    });
+})};
