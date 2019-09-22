361b37d4c8acc1900b3c8d05420856d43750edc7
Paola Stone <paola.stonemartinez@dell.com>
2019-09-22 15:28:40 -0400

    fixed indentation

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,14 +3,8 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "12345678",
   database: "bamazonDB"
 });
@@ -20,14 +14,11 @@ connection.connect(function(err) {
   displayAll();
 });
 
-
-
-
 function displayAll() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
       console.log("");
-      console.log("                    Inventory")
+      console.log("                    Inventory");
       console.log("---------------------------------------------------");
       console.log("");
       
@@ -42,7 +33,6 @@ function displayAll() {
     console.log("----------------------------------------------------");
     console.log("");
     customerChoice();
-    // connection.end();
   });
 };
 
@@ -58,10 +48,9 @@ function customerChoice() {
             return true;
           }
           // return false;
-          // connection.end();
+          connection.end();
           console.log("");
           console.log("This is not a valid entry, please start process from beginning");
-          customerChoice();
         }
       },
       {
@@ -84,22 +73,22 @@ function customerChoice() {
       connection.query(query, { item_id: answer.item_id}, function(err, res) {
         if (err) throw err;
           for (var i = 0; i < res.length; i++) {
-            console.log("")
-            console.log(" This is what we have in inventory for this product")
+            console.log("");
+            console.log(" This is what we have in inventory for this product");
             console.log("---------------------------------------------------");
             console.log("Product:       " + res[i].product_name);
             console.log("Price:         $" + res[i].price.toFixed(2));
             console.log("Qty. in stock: " + res[i].stock_quantity);
             console.log("");
             if (answer.quantity > res[i].stock_quantity){
-              console.log("---------------------------------------------------")
-              console.log("***Sorry, we do not have the quantity you need***")
-              console.log("---------------------------------------------------")
+              console.log("---------------------------------------------------");
+              console.log("***Sorry, we do not have the quantity you need***");
+              console.log("---------------------------------------------------");
               connection.end();
             }else{
               customerTotal = answer.quantity * res[i].price
               quantityUpdate = res[i].stock_quantity - answer.quantity
-              console.log("")
+              console.log("");
               console.log("---------------------------------------------------");
               console.log("This is the total of your purchase: $" + customerTotal);
               console.log("---------------------------------------------------");
@@ -114,8 +103,8 @@ function customerChoice() {
                     }
                   ],
               );
-                console.log("")
-                console.log("")
+                console.log("");
+                console.log("");
                 console.log(" This is what we have in inventory after this purchase")
                 console.log("---------------------------------------------------");
                 console.log("Product:       " + res[i].product_name);
@@ -123,10 +112,8 @@ function customerChoice() {
                 console.log("Qty. in stock: " + quantityUpdate);
                 console.log("");
                 connection.end();
-              
-            }
-          
-        };
-        
+            };
+          };
+      });
     });
-})};
+};
